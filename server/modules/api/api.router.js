c1c345e4d43f25f5d97ee8b97cf8b51b562a1045
Minh Thanh <86462628+ngthachminhthanh@users.noreply.github.com>
2024-12-11 20:33:29 +0700

    write API for Home page (#2)

--- server/modules/api/api.router.js
+++ server/modules/api/api.router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const apiController = require('./api.controller');
+const prefixProducts = '/products';
 
 // Endpoint API router for User
-// ...
+router.get(prefixProducts, apiController.getAllProducts);
+router.get(`${prefixProducts}/:category`, apiController.getProductsBaseOnCategory);
 
 // Endpoint API router for Admin
 // ...
