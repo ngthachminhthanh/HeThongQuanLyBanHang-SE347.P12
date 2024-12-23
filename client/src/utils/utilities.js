bec1eeed9543cfd6154a208d659b1f8ad510fdc0
nkthang2003 <nkthang2003@gmail.com>
2024-12-23 23:36:08 +0700

    Translate status to Vietnamese

--- client/src/utils/utilities.js
+++ client/src/utils/utilities.js
@@ -1,18 +1,35 @@
 const getCategoryName = (category) => {
-	switch (category) {
-		case 'AoNu':
-		    return 'Áo nữ';
-		case 'AoNam':
-			return 'Áo nam';
-		case 'QuanVayNu':
-			return 'Quần / Váy nữ';
-		case 'QuanNam':
-			return 'Quần nam';
-		case 'PhuKien':
-			return 'Phụ kiện';
-		default:
-			return category;
-	}
+    switch (category) {
+        case "AoNu":
+            return "Áo nữ";
+        case "AoNam":
+            return "Áo nam";
+        case "QuanVayNu":
+            return "Quần / Váy nữ";
+        case "QuanNam":
+            return "Quần nam";
+        case "PhuKien":
+            return "Phụ kiện";
+        default:
+            return category;
+    }
 };
 
 export { getCategoryName };
+
+const getStatusName = (status) => {
+    switch (status) {
+        case "waiting for confirmation":
+            return "Đang chờ xác nhận";
+        case "shipping":
+            return "Đang vận chuyển";
+        case "delivered":
+            return "Đã giao";
+        case "cancelled":
+            return "Đã hủy";
+        default:
+            return status;
+    }
+};
+
+export { getCategoryName, getStatusName };
