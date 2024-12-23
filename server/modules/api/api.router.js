7d57cc8888bd696968bf8185723b68c5d1f829ad
nkthang2003 <nkthang2003@gmail.com>
2024-12-23 23:53:36 +0700

    add endpoint API router for admin

--- server/modules/api/api.router.js
+++ server/modules/api/api.router.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const apiController = require('./api.controller');
-const prefixProducts = '/products';
+const apiController = require("./api.controller");
+const prefixProducts = "/products";
+const prefixAdmin = "/admin";
 
 // Endpoint API router for User
 router.get(prefixProducts, apiController.getAllProducts);
 router.get(`${prefixProducts}/:category`, apiController.getProductsBaseOnCategory);
 
 // Endpoint API router for Admin
-// ...
+router.get(`${prefixAdmin}/export/:dataType`, apiController.exportFile);
+router.get(`${prefixAdmin}/orders`, apiController.getAllOrders);
+router.patch(`${prefixAdmin}/orders/:id`, apiController.updateOrderStatus);
 
 module.exports = router;
