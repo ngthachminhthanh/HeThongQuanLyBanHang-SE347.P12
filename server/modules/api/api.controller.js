737ee527dcb939e66f1f4db4c5f071b661e36ba3
Sang Quang <sangquangaceraspire3@example.com>
2024-12-23 23:43:49 +0700

    handled data and completed fetching api

--- server/modules/api/api.controller.js
+++ server/modules/api/api.controller.js
@@ -4,8 +4,42 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // API handle for User
-// ...
+exports.handleOrder = async (req, res) => {
+    try {
+        const { email, address, province, district, ward, note, items, total_price } = req.body;
 
+        if (!Array.isArray(items)) {
+            return res.status(400).json({ message: 'Invalid items data' });
+        }
 
+        const newOrder = {
+            orderId: new mongoose.Types.ObjectId().toString(),
+            total_price,
+            date_order: new Date(),
+            address: `${address}, ${province}, ${district}, ${ward}`,
+            products: items.map(item => ({
+                name: item.product_name,
+                price: item.product_price,
+                quantity: item.quantity
+            })),
+            payment: {
+                date_payment: new Date(),
+                method: 'Cash on Delivery'
+            },
+            status: 'waiting for confirmation',
+            note
+        };
+
+        let customer = await CustomerEntity.findOne({ email });
+
+        customer.orders.push(newOrder);
+        await customer.save();
+
+        res.status(200).send("Order successfully!!");
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error creating order' });
+    }
+};
 // API handle for Admin
 // ...
--- server/modules/api/api.router.js
+++ server/modules/api/api.router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Endpoint API router for User
-// ...
+router.post('/order', apiController.handleOrder);
 
 // Endpoint API router for Admin
 // ...
