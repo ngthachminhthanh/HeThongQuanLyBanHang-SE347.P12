e628473700b29850ed5f164af648e16485ac7047
Nguyễn Đặng Thanh Huy <102576374+21522149@users.noreply.github.com>
2024-12-27 03:06:06 +0700

    Add new status for Zalopay (#25)
    * Add new status for Zalopay

--- client/src/utils/utilities.js
+++ client/src/utils/utilities.js
@@ -17,16 +17,24 @@ const getCategoryName = (category) => {
 
 const getStatusName = (status) => {
     switch (status) {
-        case "waiting for confirmation":
-            return "Đang chờ xác nhận";
-        case "shipping":
-            return "Đang vận chuyển";
-        case "delivered":
-            return "Đã giao";
-        case "cancelled":
-            return "Đã hủy";
-        default:
-            return status;
+		case 'waiting for confirmation':
+		    return 'Đang chờ xác nhận';
+		case 'shipping_paid':
+			return 'Đơn hàng Zalopay đang vận chuyển đã thanh toán';
+		case 'shipping':
+			return 'Đơn hàng COD đang vận chuyển'
+		case 'shipping_unpaid':
+			return 'Đơn hàng Zalopay đang vận chuyển chưa thanh toán'
+		case 'delivered':
+			return 'Đã giao';
+		case 'cancelled':
+			return 'Đã hủy';
+		case 'paid':
+			return 'Đã thanh toán';
+		case 'waiting for payment':
+			return 'Đang chờ thanh toán';
+		default:
+			return status;
     }
 };
 
