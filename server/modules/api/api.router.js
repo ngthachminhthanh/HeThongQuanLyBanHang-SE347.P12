d8bba180fa277b739d7095dd6f6a9b2968c184a4
LeTrungKienMMTT2021 <21520308@gm.uit.edu.vn>
2024-12-26 14:11:21 +0700

    Add router for GetMyOrder & getCustomer

--- server/modules/api/api.router.js
+++ server/modules/api/api.router.js
@@ -5,11 +5,10 @@ const prefixProducts = "/products";
 const prefixAdmin = "/admin";
 
 // Endpoint API router for User
-
 router.post('/order', apiController.handleOrder);
 router.get(prefixProducts, apiController.getAllProducts);
 router.get(`${prefixProducts}/:category`, apiController.getProductsBaseOnCategory);
-
+router.get('/myorders/:email', apiController.getMyOrders);
 
 // Endpoint API router for Admin
 router.get(`${prefixAdmin}/export/:dataType`, apiController.exportFile);
@@ -19,5 +18,6 @@ router.get(`${prefixAdmin}/products`, apiController.getProductsBaseOnCurrentPage
 router.post(`${prefixAdmin}/products`, apiController.addNewProduct);
 router.put(`${prefixAdmin}/products/:id`, apiController.updateProduct);
 router.delete(`${prefixAdmin}/products/:id`, apiController.deleteProduct);
+router.get(`${prefixAdmin}/customers`, apiController.getCustomers);
 
 module.exports = router;
