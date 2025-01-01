f198f3b44026f6839f555def31281a7780bd9fbc
Minh Thanh <86462628+ngthachminhthanh@users.noreply.github.com>
2025-01-02 03:37:05 +0700

    fix redirect url and cors bug (#31)
    * product management for admin
    
    * write API for product management
    
    * fix redirect url and cors bug

--- server/server.js
+++ server/server.js
@@ -12,7 +12,12 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Chỉ cho phép từ nguồn này
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Các phương thức cho phép
+  allowedHeaders: ['Content-Type', 'Authorization'], // Các header được phép
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
